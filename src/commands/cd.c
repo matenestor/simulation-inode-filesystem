90e520fde94a07f657e91bf5c43b291844a48e9b
Jakub Matejka <matejka.jk@email.cz>
2021-02-10 21:26:36 +0100

    [FIX] cd only to directory inodes

--- src/commands/cd.c
+++ src/commands/cd.c
@@ -17,38 +17,43 @@ static int handle_prompt_change() {
 	strncpy(buff_pwd, new_pwd, STRLEN_PWD_LENGTH);
 	// whole prompt change
 	snprintf(buff_prompt, BUFFER_PROMPT_LENGTH, FORMAT_PROMPT, fs_name, buff_pwd);
-    return 0;
+	return 0;
 }
 
 /*
  * Change directory in filesystem.
  */
 int sim_cd(const char* path) {
-    int ret = RETURN_FAILURE;
-    // +2 if path == 0, then SEPARATOR and \0 will be copied
+	// +2 if path == 0, then SEPARATOR and \0 will be copied
 	size_t path_length = strlen(path);
-    char target[path_length + 2];
-    struct inode inode_cd;
+	char target[path_length + 2];
+	struct inode inode_cd;
 
-    log_info("cd: [%s]", path);
+	log_info("cd: [%s]", path);
 
     // target is either path, or root, when no path is given
-    if (path_length > 0) {
+	if (path_length > 0) {
 		strncpy(target, path, path_length + 1);
-    } else {
+	} else {
 		strncpy(target, SEPARATOR, 2);
-    }
+	}
 
-    if (get_inode(&inode_cd, target) != RETURN_FAILURE) {
-        // change directory
-        memcpy(&in_actual, &inode_cd, sizeof(struct inode));
-        handle_prompt_change();
-        ret = RETURN_SUCCESS;
-    }
-    else {
-        set_myerrno(Err_item_not_exists);
-        log_warning("cd: unable to change directory to [%s]", path);
-    }
+	if (get_inode(&inode_cd, target) == RETURN_FAILURE) {
+		set_myerrno(Err_item_not_exists);
+		goto fail;
+	}
+	if (inode_cd.inode_type != Inode_type_dirc) {
+		set_myerrno(Err_item_not_directory);
+		goto fail;
+	}
 
-    return ret;
+	// change directory
+	memcpy(&in_actual, &inode_cd, sizeof(struct inode));
+	handle_prompt_change();
+
+	return RETURN_SUCCESS;
+
+fail:
+	log_warning("cd: unable to change directory to [%s]", path);
+	return RETURN_FAILURE;
 }
